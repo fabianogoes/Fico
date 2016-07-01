09d74ceca0224fc76d4b5d0e37bb40cd53d837fd
Fabiano Góes <fgoes@dellavolpe.com.br>
2016-07-01 16:41:01 -0300

    create/settings/layout project

---
+++ src/main/java/br/com/fico/FicoApplication.java
@@ -0,0 +1,12 @@
+package br.com.fico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FicoApplication.class, args);
+	}
+}
